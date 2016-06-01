243729fd13441f9e304a310205aa66978acc578d
Bearisas <m.aramavicius@gmail.com>
2016-06-01 12:32:32 +0300

    link support

--- public/js/message_processor.js
+++ public/js/message_processor.js
@@ -1,14 +1,15 @@
 function __processmessage(msg){
-	//LINK PROCESS
+	
 	var last_pos = 0;
 	var timeout = 0;
 	var newmsg = msg;
 	while(last_pos < msg.length){
 		console.log(last_pos);
 		timeout++;
-		if(timeout > 10){
+		if(timeout > 500){
 			break;
 		}
+		//LINK PROCESS
 		if(msg.indexOf("http", last_pos) > -1 || msg.indexOf("www", last_pos) > -1){
 			var h = msg.indexOf('http', last_pos);
 			var w = msg.indexOf('www', last_pos);
@@ -23,15 +24,45 @@ function __processmessage(msg){
 				start = w;
 			}
 			var end = msg.indexOf(" ",start) != -1 ? msg.indexOf(" ",start) : msg.length;
+
+			//SEPERATE IF TWO LINKS W/O SPACE
+			if(msg.indexOf("http",start+"http".length) > -1 || msg.indexOf("www", start+"www".length) > -1){
+				var s2h = msg.indexOf("http",start+"http".length);
+				var s2w = msg.indexOf("www", start+"www".length);
+				s2h = s2h == -1 ? end : s2h;
+				s2w = s2w == -1 ? end : s2w;
+				if(s2h < end || s2w < end){
+					if(s2h <= s2w && s2h != -1){
+						end = s2h;
+					}
+					else if(s2w <= s2h && s2w != -1 && s2w > start + "https://".length){
+						end = s2w;
+					}
+				}
+			}
+			//--------
+
 			var __link = msg.substr(start, end-start);
+			var gotolink = __link;
+			//ADD HTTP
+			if(start == w){
+				gotolink = "http://"+gotolink;
+			}
+			//-------
+
+
+
 			var __formatted = "";
+			//TRY IMAGE
 			if(__link.indexOf(".jpg") > -1 || __link.indexOf(".gif") > -1 || __link.indexOf(".png") > -1 || __link.indexOf(".bmp") > -1){
-				__formatted = "<a href='"+__link+"' target='blank'><img src='"+__link+"' style='max-width:100px; max-height:100px;'/></a>";
+				__formatted = "<a href='"+gotolink+"' target='blank'><img src='"+gotolink+"' style='max-width:100px; max-height:100px;'/></a>";
 			}
 			else{
-				__formatted = "<a href='"+__link+"' target='_blank'>"+__link+"</a>";
+				__formatted = "<a href='"+gotolink+"' target='_blank'>"+__link+"</a>";
 			}
-			console.log(__formatted);
+			//-----
+
+
 			newmsg = newmsg.replace(__link, __formatted);		
 			last_pos = end;
 			continue;
